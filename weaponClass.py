69b93a79fffe63c9b700851fcd1a8f6c0b448097
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-02-16 15:28:08 -0600

    Program is working!

--- weaponClass.py
+++ weaponClass.py
@@ -1,10 +1,43 @@
 import random
 
+class Weapon:
+
+    def __init__(self, name, speed, range1):
+        self.__name = name
+        self.__speed = speed
+        self.__range = range1
+        self.__bullets = 0
+        self.__status = 'Active'
+
+    def count_bullets(self):
+        self.__bullets = random.randint(0, 10) 
+
+    def fire_bullet(self):
+        if self.__bullets != 0:
+            self.__bullets -= 1
+            print('Updated Number of Bullets:\t\t', self.__bullets)
+        else:
+            self.__status = 'Inactive'
+            print('Updated Status:\t\t\t\t', self.__status)
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_speed(self):
+        return self.__speed
+    
+    def get_range1(self):
+        return self.__range
+
+    def get_bullets(self):
+        return self.__bullets
+    
+    def get_status(self):
+        return self.__status
+
 '''
 Create a Weapon Class definition according to the following specs:
-
 Attributes:
-
 name - user supplied
 bullets - random number between 10 and 100000
 speed - user supplied
@@ -12,10 +45,7 @@ range - user supplied
 status - 'Active' (this attribute should be changed to 'Inactive' if bullets 
                     run out.)
 
-Methods:
-
-Create accessor methods for each attribute.
-
+Methods:Create accessor methods for each attribute.
 Create a method named 'fire_bullet' that will simulate
 firing a bullet. This is accomplished by decreasing the number of bullets by 1 
 every time the method is called. When the bullet count reaches zero, it should change
--- weaponProgram.py
+++ weaponProgram.py
@@ -1,7 +1,5 @@
 import weaponClass as w
 import csv
-
-
 '''
 - Craete a program that will read the contents of the file 'weapons.txt'. Each record in the file represents the specs to a weapon.
 - Create an instance of the weapon object for each record. 
@@ -9,69 +7,52 @@ import csv
 - Print out details of each weapon using the object's methods only (as per comments below). 
 - Fire all bullets of the weapon and print a countdown of bullets remaining (run exe file to visualize, HINT: use end='\r' in your print statement).
 - Print out the name of the weapon and the number of bullets from the dictionary.
-
-HINT: Follow the comments for each line to help with the logic of the problem.
 '''
-
-
 # create a file object to open the file in read mode
-
-
-
-# create a csv object from the file object
-
-
+content = open('weapons.csv', 'r')
+content_file = csv.reader(content, delimiter=',')
 
 #skip the header row
-
-
-
+next(content_file)
 
 #create an empty dictionary named 'weapons_dict'
-
-
-
-
+weapons_dict = {}
 #use a for loop to iterate through every row of the csv file
-
+for record in content_file:
     #use variables for name,speed and range (optional)
-    
-
-    # create an instance of the weapon object using the 
-    # specs from the csv file (name,speed and range) 
-    
+    name = record[0]
+    speed = record[1]
+    range1 = record[2]
+    # create an instance of the weapon object using the specs from the csv file (name,speed and range) 
+    my_weapon = w.Weapon(name, speed, range1)
+    my_weapon.count_bullets()
 
     # append the name and bullet count to 'weapons_dict'
-    
-
+    weapons_dict[name] = my_weapon.get_bullets()
 
     # print out the name of the weapon using the appropriate method of the object 
-    
+    print('Name: ', my_weapon.get_name())
     # print out the speed of the weapon using the appropriate method of the object
-    
+    print('Speed: ', my_weapon.get_speed())
     # print out the range of the weapon using the appropriate method of the object
-    
+    print('Range: ', my_weapon.get_range1())
     # print out the number of bullets of the weapon using the appropriate method of the object
-    
+    print('Initial Number of Bullets: ', my_weapon.get_bullets())
 
     #use an input statement to halt the program and wait for the user - 
     input("Press any key to fire the weapon")
-    
 
     # use an appropriate loop to keep firing the weapon until all bullets run out
-    
+    while my_weapon.get_status() == 'Active':
         # call the appropriate method to fire a bullet
-       
+        my_weapon.fire_bullet()
         # print out the bullet count every time the weapon is fired
-        
-
     
-
-
 #using a loop print out the name and number of bullets from the dictionary
-
-
-
+for key, value in weapons_dict.items():
+    print('Name: ', key, '\t\tInitial Number of Bullets: ', value)
+# questions: do you want the initial number of bullets to be pritned out in the dictionary?
+# I print out the bullet count each time from the class, is this ok? instead of following your instructions
 
     
 
